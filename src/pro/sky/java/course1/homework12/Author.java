01bebbd0961b12141bbdcce659a9f76d8b5aa493
ПК <ekrapostin@gmail.com>
2023-01-09 23:45:11 +0300

    Дз готово

---
+++ src/pro/sky/java/course1/homework12/Author.java
@@ -0,0 +1,14 @@
+package pro.sky.java.course1.homework12;
+
+public class Author {
+    private String name;
+
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
